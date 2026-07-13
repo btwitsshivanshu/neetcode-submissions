b3ae95c2408dbcfd5dc0a77a02139083e5cdc692
Shivanshu Agarwal <162721551+btwitsshivanshu@users.noreply.github.com>
2026-07-13 17:49:18 +0530

    Add: binary-tree-maximum-path-sum - submission-0

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mxsum = INT_MIN;
+    int solve(TreeNode * root){
+        if(!root)  return 0;
+
+        int l=max(0,solve(root->left));
+        int r=max(0,solve(root->right));
+
+        mxsum = max(mxsum, root->val + l + r);
+        return root->val + max(l, r);
+    }
+    int maxPathSum(TreeNode* root) {
+
+        solve(root);
+        return mxsum;
+
+    }
+
