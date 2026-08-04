5e0dc78fcddc17d7f3b2e1250d8f814fa654ef8a
Shivanshu Agarwal <162721551+btwitsshivanshu@users.noreply.github.com>
2026-08-04 07:37:17 +0530

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    // int solve(int i, int j, int m , int n){
+
+    //     if(i<0 || i>=m || j<0 || j>=n){
+    //         return 0;
+    //     }
+
+    //     if(i==m-1 && j==n-1){
+    //         return 1;
+    //     }
+
+    //     int down= solve(i+1,j,m,n);
+    //     int right= solve(i,j+1,m,n);
+
+    //     return down+right;
+    // }
+    int uniquePaths(int m, int n) {
+
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+        vector<int>curr(n+1);
+        vector<int>next(n+1);
+
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+
+                if(i==m-1 && j==n-1){
+                    curr[j]= 1;
+                }
+
+                else{
+                    int down= next[j];
+                    int right= curr[j+1];
+
+                    curr[j]=down+right;
+
+                }
+
+            }
+            next=curr;
+        }
+
+        return next[0];
+
+        
+    }
+};
